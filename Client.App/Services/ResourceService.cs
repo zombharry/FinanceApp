13f77eba9a26828115420d9fc5b0a161770f6290
Zsombor Bihari <zsomobor@gmail.com>
2026-07-22 15:49:58 +0200

    AddItem added

--- Client.App/Services/ResourceService.cs
+++ Client.App/Services/ResourceService.cs
@@ -44,4 +44,52 @@ public class ResourceService
 
         response.EnsureSuccessStatusCode();
     }
+
+    public async Task<ProductGetDTO> GetProductById(Guid productId)
+    {
+        var response = await _apiService.GetAsync($"api/product/getbyid?productId={productId}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        await using var responseStream = await response.Content.ReadAsStreamAsync();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var product = await JsonSerializer.DeserializeAsync<ProductGetDTO>(responseStream, options);
+
+        return product;
+    }
+
+    public async Task EditProduct(ProductEditDTO item)
+    {
+        var response = await _apiService.PostDataAsync("api/product/edit", item);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task DeleteItem(Guid itemIdentifier) { }
+
+    public async Task<IEnumerable<CategoryGetDTO>> GetCategories()
+    {
+        var response = await _apiService.GetAsync("api/Category/getall");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return Enumerable.Empty<CategoryGetDTO>();
+        }
+
+        await using var responseStream = await response.Content.ReadAsStreamAsync();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var categories = await JsonSerializer.DeserializeAsync<IEnumerable<CategoryGetDTO>>(responseStream, options);
+
+        return categories ?? Enumerable.Empty<CategoryGetDTO>();
+    }
 }
